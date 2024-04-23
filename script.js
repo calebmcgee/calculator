ef0db26fa60268dd2ba3c7543dce41a2e6c32a2d
Caleb <calebmcgee06@gmail.com>
2024-04-23 12:23:05 -0400

    Create operate function for calculator inputs

--- script.js
+++ script.js
@@ -3,3 +3,21 @@ const subtract = (...args)  => args.reduce((difference, num) => difference - num
 const multiply = (...args)  => args.reduce((product, num) => product * num);
 const divide = (...args)  => args.reduce((quotient, num) => quotient / num);
 
+const operate = (num1, operator, num2) => {
+    switch (operator){
+        case '+' :  
+            console.log(add(num1,num2));
+            break;
+        case '-' :
+            console.log(subtract(num1,num2));
+            break;
+        case '*' :
+            console.log(multiply(num1,num2));
+            break;
+        case '/' :
+            console.log(divide(num1,num2));
+            break;
+    }
+}
+
+
