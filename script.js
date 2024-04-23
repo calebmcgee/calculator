aa33f63dc07fcfe8e15595264a8ab045af43ad74
Caleb <calebmcgee06@gmail.com>
2024-04-23 12:21:11 -0400

    Create functions for basic operations +,-,*,/

---
+++ script.js
@@ -0,0 +1,5 @@
+const add = (...args)  => args.reduce((sum, num) => sum + num);
+const subtract = (...args)  => args.reduce((difference, num) => difference - num);
+const multiply = (...args)  => args.reduce((product, num) => product * num);
+const divide = (...args)  => args.reduce((quotient, num) => quotient / num);
+
